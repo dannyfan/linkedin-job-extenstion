85d5708f82923d282b16efc2f6a2bd38d93d0611
Danny Fan <dannyfan@live.com>
2020-02-25 22:23:09 -0500

    fix: showing all of job description without clicking see more

--- main.js
+++ main.js
@@ -56,24 +56,39 @@ function sortJobsByTime() {
 
 function openLinksNewTab() {
     const jobLinks = document.querySelector('a.job-card__link-wrapper');
-    jobLinks.addEventListener('click', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        const link = this.getAttribute('href');
-        window.open(link, '_blank');
-    });
+    if (jobLinks) {
+        jobLinks.addEventListener('click', function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+            const link = this.getAttribute('href');
+            window.open(link, '_blank');
+        });
+    }
+}
+
+function showFullJobDescription() {
+    const button = document.querySelector('button[data-control-name="see_more"]');
+    const jobDescription = document.querySelector('.jobs-description--is-truncated');
+    console.log(button);
+    console.log(jobDescription);
+    if (button && jobDescription) {
+        console.log('click');
+        button.click();
+    }
 }
 
 function createSortButton() {
     const container = document.querySelector('.ph5.pb0.pt2');
-    const button = document.createElement('button');
-    const buttonText = document.createTextNode('Sort Jobs (Newest - Oldest)');
-    button.appendChild(buttonText);
-    button.classList.add('artdeco-button');
-    button.addEventListener('click', function() {
-        sortJobsByTime();
-    });
-    container.appendChild(button);
+    if (container) {
+        const button = document.createElement('button');
+        const buttonText = document.createTextNode('Sort Jobs (Newest - Oldest)');
+        button.appendChild(buttonText);
+        button.classList.add('artdeco-button');
+        button.addEventListener('click', function() {
+            sortJobsByTime();
+        });
+        container.appendChild(button);
+    }
 }
 
 (function() {
@@ -83,11 +98,28 @@ function createSortButton() {
     }
 
     const observer = new MutationObserver(function(mutationsList, observer) {
-        hideMessageBar();
-        hidePremiumAd();
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                if (mutation.target.classList.contains('msg-overlay-list-bubble')) {
+                    hideMessageBar();
+                }
+                if (mutation.target.classList.contains('gp-promo-embedded-card-2__card')) {
+                    hidePremiumAd();
+                }
+                // if (mutation.target.classList.contains('application-outlet') && !clicked) {
+                //     console.log('job description here');
+                //     showFullJobDescription();
+                //     clicked = true;
+                // }
+            }
+        }
     });
 
     observer.observe(document, config);
     createSortButton();
     openLinksNewTab();
+
+    window.onload = function() {
+        showFullJobDescription();
+    }
 })();
